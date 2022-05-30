2377ab741d979f8a0b089ac668243bf2f7174b03
Edouard Gauvrit <60110891+Egauvrit@users.noreply.github.com>
2022-05-30 19:45:57 +0200

    Update and rename wstoa.py to woast.py
    typo corrections.

--- woast.py
+++ woast.py
@@ -4,12 +4,30 @@ import dask.array as dka
 import warnings
 warnings.simplefilter(action='ignore', category=dka.PerformanceWarning)
 
+def Hvelmag(ds,var=('U','V'),name=None):
+    da1,da2 = [ds[v] for v in var]
+    if name == None:
+        name = 'Hvel'
+    func = lambda x, y: np.sqrt(x ** 2 + y ** 2)
+    #return xr.apply_ufunc(func,da1,da2,dask='allowed')
+    return ds.assign({name:xr.apply_ufunc(func,da1,da2,dask='allowed')})
 
-def WST(arr,Ntht,scales,xydim,var=None,normalize=True):
+def Flux(ds,var,mdim=('level','nj','ni'),name=None):
+    if len(var)!=2:
+        raise ValueError("var must contain 2 arguments, {:1d} given.".format(len(var)))
+    elif type(var[0])!= str:
+        raise SyntaxError("var must contain only str objects.")
+    if name == None:
+        name = (var[0]+var[1]).lower()
+    da1,da2 = [ds[v] for v in var]
+    return ds.assign({name:(da1-da1.mean(dim=mdim)) * (da2-da2.mean(dim=mdim))})
+
+def woast(arr,Ntht,scales,xydim,var=None,normalize=True):
 	tmp = WST(arr,Ntht,scales,xydim,var)
 	if normalize==True:
 		tmp.normalize()
 	return tmp.data
+
 
 class WST():
     """docstring for WSTOA"""
